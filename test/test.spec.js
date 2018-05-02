422fb338485c0f221430a1ff742b9572ed101daa
DRL\caffet <tom.cafferkey@ao.com>
2018-05-02 11:20:22 +0100

    Initial cookie utility function commit

---
+++ test/test.spec.js
@@ -0,0 +1,12 @@
+"use strict";
+const expect = require("chai").expect;
+const assert = require("chai").assert;
+const cookie = require("../lib/index").cookie;
+
+describe('testing cookie() function to safely set cookie values', function()  {
+
+	it('should have cookie named testCookie', function() {
+
+	});
+
+});
